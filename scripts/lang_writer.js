547debbb40408266af5823c3f98fa42528ac1e46
Andreiskate2 <andrei.ungur@mail.mcgill.ca>
2017-03-11 22:43:32 -0500

    Mass changes to web page
    Re-factored all the code, made stylistic changes and included handling of two languages

---
+++ scripts/lang_writer.js
@@ -0,0 +1,109 @@
+var ENG;
+var FR;
+var lang_data = {"EN":"","FR":""};
+var language;
+
+//Set-up on document start
+$(document).ready(function() {
+	$.ajaxSetup({ cache: false });
+});
+
+$.ajaxSetup({
+	cache: false
+});
+
+//Get data from english JSON file
+$.getJSON( "./scripts/content/english.json"+'?', { cache: false},function() {})
+.done(function( data ) {
+	ENG = data;
+	lang_data.EN = ENG;
+})
+.fail( function(d, textStatus, error) {
+	console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+})
+.always(function() {
+});
+
+//Get data from french JSON file
+$.getJSON( "./scripts/content/french.json"+'?', { cache: false},function() {})
+.done(function( data ) {
+	FR = data;
+	lang_data.FR=FR;
+})
+.fail( function(d, textStatus, error) {
+	console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+})
+.always(function() {
+});
+
+//Write data based on user selection
+$('#FR-Select').click(function($e){
+	$e.preventDefault();
+	writeData("FR");
+});
+
+$('#ENG-Select').click(function($e){
+	$e.preventDefault();
+	writeData("EN");
+});
+
+//Defines which id's map to which spots in the JSON file
+function writeData(lang){
+	language = lang;
+	//About
+	$('#am').text(lang_data[lang].about);
+	$('#a_p1').text(lang_data[lang].about_1);
+	$('#a_p2').text(lang_data[lang].about_2);
+	$('#a_p3').text(lang_data[lang].about_3);
+	
+	//Education
+	$('#ed').text(lang_data[lang].education);
+	$('#mcgill_edu').text(lang_data[lang].mcgill_edu);
+	$('#mcgill_desc_1').text(lang_data[lang].mcgill_desc[0]);
+	$('#mcgill_desc_2').text(lang_data[lang].mcgill_desc[1]);
+	$('#mcgill_desc_3').text(lang_data[lang].mcgill_desc[2]);
+	$('#breb_edu').text(lang_data[lang].breb_edu);
+	$('#breb_desc_1').text(lang_data[lang].breb_desc[0]);
+	$('#breb_desc_2').text(lang_data[lang].breb_desc[1]);
+	$('#breb_desc_3').text(lang_data[lang].breb_desc[2]);
+	$('#beaubois_edu').text(lang_data[lang].beaubois_edu);
+	$('#beaubois_desc_1').text(lang_data[lang].beaubois_desc[0]);
+	$('#beaubois_desc_2').text(lang_data[lang].beaubois_desc[1]);
+	$('#beaubois_desc_3').text(lang_data[lang].beaubois_desc[2]);
+
+	//Experience
+	$('#ex').text(lang_data[lang].experience);
+	//Activities
+	$('#activities').text(lang_data[lang].activities);
+	$('#act-intro').text(lang_data[lang].activities_1);
+	$('#act-1').text(lang_data[lang].activities_list[0]);
+	$('#act-2').text(lang_data[lang].activities_list[1]);
+	$('#act-3').text(lang_data[lang].activities_list[2]);
+	$('#act-code').text(lang_data[lang].activities_code);
+	
+	//Work
+	var worklabel = "work";
+	$('#'+worklabel).text(lang_data[lang][worklabel]);
+	for(var i=0;i<3;i++){
+		$('#'+(worklabel+"-"+(i+1))).text(lang_data[lang][(worklabel+'_'+i)]);
+		for(var j=0;j<lang_data[lang][(worklabel+"_"+i)].length;j++){
+			$('#'+worklabel+"-"+(i+1)+"-"+(j+1)).text(lang_data[lang][(worklabel+"_"+i+"_list")][j]);
+		}
+	}
+	
+	//Volunteering
+	$('#volunteering').text(lang_data[lang].volunteering);
+	$('#vol').text(lang_data[lang].volunteering_1);
+	$('#vol-1').text(lang_data[lang].volunteering_list[0]);
+	$('#vol-2').text(lang_data[lang].volunteering_list[1]);
+	
+	//Projects
+	$('#pj').text(lang_data[lang].projects);
+	$('#projects_intro').text(lang_data[lang].projects_contact);
+	$('#projects_more').text(lang_data[lang].projects_about);
+	
+	for(var i=0;i<3;i++){
+		$('#proj-'+(i+1)+'-title').text(lang_data[lang]["projects_"+i][0]);
+		$('#proj-'+(i+1)+'-description').text(lang_data[lang]["projects_"+i][1]);
+	}
+}
---
+++ scripts/name_validation.js
@@ -0,0 +1,50 @@
+function setName(){
+  var user_name = $('#name_input').val();
+  var text_field = $('#custom_text');
+
+  var output_text = "";
+  var is_valid = true;
+
+  //Some basic input validation
+  if (user_name!=null){
+    for (i=0;i<user_name.length-1;i++){
+      if (!(user_name.toUpperCase().charAt(i)>='A' && user_name.toUpperCase().charAt(i)<='Z')){
+          if (user_name.charAt(i)==' '){ //Hyphens and spaces are accepted
+            continue;
+          } else if(user_name.charAt(i)=='-'){
+            continue;
+          } else {
+            is_valid=false;
+          }
+        }
+      }
+      if (!is_valid){
+        if(language == "EN"){
+          output_text = "You have entered an invalid name! ";
+        } else {
+          output_text = "Vous n'avez pas entré un nom valide!"
+        }
+        return;
+      } else {
+        localStorage.setItem('user', user_name);
+        if(language == "EN"){
+          output_text = "Hi "+user_name+"! ";         
+        } else{
+          output = "Salut "+user_name+"! ";
+        }
+      }
+    } else {
+      if(language == "EN"){
+        output_text = "Hi "+localStorage.getItem('user')+" ! ";
+      } else {
+        output_text = "Salut "+localStorage.getItem('user')+"! ";
+      }
+    }
+    if(language == "EN"){
+      output_text+="This page was created by Andrei Ungur, a second year Software Engineering student of McGill University, using Github/HTML5/JQuery/Javascript/CSS/Boostrap/Angularjs. Click on any of the tabs above for more information.";    
+   } else {
+      output_text+="Cette page a été crée par Andrei Ungur, un étudiant de deuxième année en Génie Logiciel à l'Université McGill, en utilisant Github/HTML5/JQuery/Javascript/CSS/Boostrap/Angularjs. Cliquez sur n'importe quel onglet pour plus d'information.";
+   }
+   
+   text_field.text(output_text);
+ }
