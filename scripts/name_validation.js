2735d561be8f50a88857e60207ac7a5b577e7045
andrei <andrei.ungur@mail.mcgill.ca>
2018-01-05 01:05:46 -0500

    Remove gimmicky name input
    It was a fun feature to implement, but it was a little bit useless so I removed it.
    Also made small stylistic changes.

--- scripts/name_validation.js
+++ scripts/name_validation.js
@@ -1,52 +1,8 @@
+const text_field = $('#intro_text');
 function setName(){
-  var user_name = $('#name_input').val();
-  var user_history = localStorage.getItem('user');
-  var text_field = $('#custom_text');
-
-  var output_text = "";
-  var is_valid = true;
-
-  //Some basic input validation
-  if (user_name!=null && user_name.length>0){
-    for (i=0;i<user_name.length-1;i++){
-      if (!(user_name.toUpperCase().charAt(i)>='A' && user_name.toUpperCase().charAt(i)<='Z')){
-          if (user_name.charAt(i)==' '){ //Hyphens and spaces are accepted
-            continue;
-          } else if(user_name.charAt(i)=='-'){
-            continue;
-          } else {
-            is_valid=false;
-          }
-        }
-      }
-
-      if (!is_valid){
-        if(language == "EN"){
-          output_text = "You have entered an invalid name! ";
-        } else {
-          output_text = "Vous n'avez pas entré un nom valide!"
-        }
-      } else {
-        localStorage.setItem('user', user_name);
-        if(language == "EN"){
-          output_text = "Hi "+user_name+"! ";         
-        } else{
-          output = "Salut "+user_name+"! ";
-        }
-      }
-    } else if(user_history!=null && user_history.length>0){
-      if(language == "EN"){
-        output_text = "Hi "+user_history+" ! ";
-      } else {
-        output_text = "Salut "+user_history+"! ";
-      }
-    }
-
     if(language == "EN"){
-      output_text+="This page was created by Andrei Ungur, a Software Engineering student of McGill University, using Github/HTML5/JQuery/Javascript/CSS/Boostrap/Angularjs. Click on any of the tabs above for more information.";    
+      text_field.text("This page was created by Andrei Ungur, a Software Engineering student of McGill University, using Github/HTML5/JQuery/Javascript/CSS/Boostrap/Angularjs. Click on any of the tabs above for more information.");    
     } else {
-      output_text+="Cette page a été crée par Andrei Ungur, un étudiant en Génie Logiciel à l'Université McGill, en utilisant Github/HTML5/JQuery/Javascript/CSS/Boostrap/Angularjs. Cliquez sur n'importe quel onglet pour plus d'information.";
+      text_field.text("Cette page a été crée par Andrei Ungur, un étudiant en Génie Logiciel à l'Université McGill, en utilisant Github/HTML5/JQuery/Javascript/CSS/Boostrap/Angularjs. Cliquez sur n'importe quel onglet pour plus d'information.");
     }
-    
-    text_field.text(output_text);
   }
--- scripts/smooth_slide.js
+++ scripts/smooth_slide.js
@@ -1,34 +1,67 @@
+const about_me_header = $('#am');
+const about_me_text = $('#about');
+
+const education_header = $('#ed');
+const education_text = $('#education');
+
+const experience_header = $('#ex');
+const experience_text = $('#experience');
+
+const projects_header = $('#pj');
+const projects_text = $('#other');
+
+const intro_text = $('#etc');
+
+function showIntroText(header){
+  intro_text.fadeIn();
+  header.css('background','white');
+}
+
+function showContent(content, header){
+  if(content != projects_text){
+    projects_header.css('background','white');
+    projects_text.hide();
+  }
+  if (content != about_me_text){
+    about_me_header.css('background','white');
+    about_me_text.hide();
+  }
+  if (content != experience_text){
+    experience_header.css('background','white');
+    experience_text.hide();
+  }
+  if (content != education_text){
+    education_header.css('background','white');
+    education_text.hide();
+  }
+  intro_text.hide();
+  content.slideToggle();
+  header.css('background','#bababa');
+}
+
 $(document).ready(function(){
-  $("#am").click(function(){
-    $("#about").slideToggle();
-    $("#etc").hide();
-    setTimeout(function(){if ($("#about").css("display")!="block"){
-      $("#etc").fadeIn();
-    }},500);
-    $("#education, #experience, #other").hide();
-  });
-  $("#ed").click(function(){
-    $("#education").slideToggle();
-    $("#about, #experience, #other").hide();
-    $("#etc").hide();
-    setTimeout(function(){if ($("#education").css("display")!="block"){
-      $("#etc").fadeIn();
+  about_me_header.click(function(){
+    showContent(about_me_text, about_me_header);
+    setTimeout(function(){if (about_me_text.css("display")!="block"){
+      showIntroText(about_me_header);
     }},500);
   });
-  $("#ex").click(function(){
-    $("#experience").slideToggle();
-    $("#education, #about, #other").hide();
-    $("#etc").hide();
-    setTimeout(function(){if ($("#experience").css("display")!="block"){
-      $("#etc").fadeIn();
+  education_header.click(function(){
+    showContent(education_text, education_header);
+    setTimeout(function(){if (education_text.css("display")!="block"){
+      showIntroText(education_header);
     }},500);
   });
-  $("#pj").click(function(){
-    $("#other").slideToggle();
-    $("#education, #experience, #about").hide();
-    $("#etc").hide();
-    setTimeout(function(){if ($("#other").css("display")!="block"){
-      $("#etc").fadeIn();
+  experience_header.click(function(){
+    showContent(experience_text, experience_header);
+    setTimeout(function(){if (experience_text.css("display")!="block"){
+      showIntroText(experience_header);
+    }},500);
+  });
+  projects_header.click(function(){
+    showContent(projects_text, projects_header);
+    setTimeout(function(){if (projects_text.css("display")!="block"){
+      showIntroText(projects_header);
     }},500);
   });
 });
