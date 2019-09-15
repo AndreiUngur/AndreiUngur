867f38cbc86419c8ab07261ac321eb2659b2681b
Andrei <andrei.ungur@mail.mcgill.ca>
2019-09-15 19:24:39 -0400

    Revamp design and include different projects
    The old UI was a bit outdated and hard to navigate. I have scrapped it
    and made a new, stripped down and simple, one.

--- README.md
+++
@@ -1,3 +0,0 @@
-# AndreiUngur.github.io
-
-My personal website started in 2015 to learn more about Git/HTML/Javascript. It has been updated relatively regularly since then.
--- scripts/name_validation.js
+++
@@ -1,8 +0,0 @@
-const text_field = $('#intro_text');
-function setName(){
-    if(language == "EN"){
-      text_field.text("This page was created by Andrei Ungur, a Software Engineering student of McGill University, using Github/HTML5/JQuery/Javascript/CSS/Boostrap/Angularjs. Click on any of the tabs above for more information.");    
-    } else {
-      text_field.text("Cette page a été crée par Andrei Ungur, un étudiant en Génie Logiciel à l'Université McGill, en utilisant Github/HTML5/JQuery/Javascript/CSS/Boostrap/Angularjs. Cliquez sur n'importe quel onglet pour plus d'information.");
-    }
-  }
